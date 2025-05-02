ed1b2f8af414821241605e8773ce1cdce4644a2d
Bruno Ananias <brunomananias2009@hotmail.com>
2025-05-02 17:37:02 -0300

    criado arquivo vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['7d78-2804-d46-3b0e-6500-13d-ee9e-7c72-6e4f.ngrok-free.app']
-  }
 })
